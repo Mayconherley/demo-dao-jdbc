96def180dfd7459cd0b8eed2e72c37fcdc01f12d
Maycon Herley <mayconherley18@gmail.com>
2025-03-30 18:39:08 -0300

    Seller entity class

---
+++ src/application/Program.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.Date;
+
+import model.entities.Department;
+import model.entities.Seller;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Book");
+		Seller seller = new Seller(21,"Dhony", "dhony@gmail.com",new Date(),2000.0,obj);
+		System.out.println(seller);
+
+	}
+
+}
