69dfb77fb3ee3692e90eba942d1f42009892486f
Maycon Herley <mayconherley18@gmail.com>
2025-04-10 13:37:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,17 +2,21 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerdDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department obj = new Department(1, "Book");
-		Seller seller = new Seller(21,"Dhony", "dhony@gmail.com",new Date(),2000.0,obj);
-		System.out.println(seller);
 
+		Department obj = new Department(1, "Book");
+		Seller seller = new Seller(21, "Dhony", "dhony@gmail.com", new Date(), 2000.0, obj);
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println(seller);
+		
 	}
 
 }
