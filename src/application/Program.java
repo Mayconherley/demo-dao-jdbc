49abd8fb822dddb16011f51f38f65310b97b1182
Maycon Herley <mayconherley18@gmail.com>
2025-04-10 15:05:38 -0300

    Implementation findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.dao.impl.SellerdDaoJDBC;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Book");
-		Seller seller = new Seller(21, "Dhony", "dhony@gmail.com", new Date(), 2000.0, obj);
+		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		Seller seller = sellerDao.FindById(3);
 		System.out.println(seller);
 		
 	}
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerdDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
-		return new SellerdDaoJDBC();
+		return new SellerdDaoJDBC(DB.getConnection());
 	}
 }
