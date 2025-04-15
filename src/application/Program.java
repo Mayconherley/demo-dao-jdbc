4c1ed0e8af88c0b6d548ff3911a2da05459b7add
Maycon Herley <mayconherley18@gmail.com>
2025-04-15 15:28:57 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -25,7 +25,10 @@ public class Program {
 		for(Seller obj : list) {
 			System.out.println(obj);
 		}
-		
+		System.out.println("\n===TEST 3: seller findByAll ===");
+	    list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+			}
 	}
-
 }
